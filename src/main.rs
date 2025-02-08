82736e3f63e19430f6c8e9cde4cf303a88c3b629
Memnoc <smartdroidesign@gmail.com>
2025-02-08 17:19:44 +0000

    multiline input implementation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-use iced::{widget::text, Element, Sandbox, Settings};
+use iced::{
+    widget::{container, text_editor},
+    Element, Sandbox, Settings,
+};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content,
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self {
+            content: text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
@@ -21,10 +30,15 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Message) {
-        todo!()
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        text("Hello, Iced!").into()
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        container(input).padding(10).into()
     }
 }
