36e9ffa21341e83ac509f608d24bb435f3134ed6
Memnoc <smartdroidesign@gmail.com>
2025-02-09 16:26:19 +0000

    initialised content from a file and async in tokyo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{io, path::Path, sync::Arc};
+
 use iced::{
     executor,
     widget::{column, container, horizontal_space, row, text, text_editor},
@@ -15,6 +17,7 @@ struct Editor {
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
 
 impl Application for Editor {
@@ -28,9 +31,12 @@ impl Application for Editor {
     fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
         (
             Self {
-                content: text_editor::Content::with(include_str!("main.rs")),
+                content: text_editor::Content::new(),
             },
-            Command::none(),
+            Command::perform(
+                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
+                Message::FileOpened,
+            ),
         )
     }
 
@@ -42,6 +48,11 @@ impl Application for Editor {
         match message {
             Message::Edit(action) => {
                 self.content.edit(action);
+            }
+            Message::FileOpened(result) => {
+                if let Ok(content) = result {
+                    self.content = text_editor::Content::with(&content);
+                }
             }
         }
         Command::none()
@@ -65,4 +76,11 @@ impl Application for Editor {
     fn theme(&self) -> Theme {
         Theme::Dark
     }
+}
+
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|error| error.kind())
 }
